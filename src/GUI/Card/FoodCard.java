b7de8c45f1a4691898874b50c34dc12853671f75
Nguyễn Nam Dương <nguyennamduong205@gmail.com>
2025-03-09 20:30:53 +0700

    Debug:

--- src/GUI/Card/FoodCard.java
+++ src/GUI/Card/FoodCard.java
@@ -3,53 +3,37 @@ package GUI.Card;
 import javax.swing.*;
 import javax.swing.plaf.DimensionUIResource;
 import java.awt.*;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 
 import GUI.Components.CustomButton;
 import Utils.Helper.Comon;
 
 public class FoodCard extends JPanel {
+    private String idProduct;
     private ImageIcon imageProduct;
     private String nameProduct;
     private String price;
-    private CustomButton addButton;
-    private CustomButton editButton;
 
-    public FoodCard(ImageIcon imageProduct, String nameProduct, String price, CustomButton addButton, CustomButton editButton) {
+    public FoodCard(ImageIcon imageProduct, String idProduct, String nameProduct, String price) {
         this.imageProduct = imageProduct != null ? imageProduct : new ImageIcon(System.getProperty("user.dir") +"/src/Assets/Food/none.png");
+        this.idProduct = idProduct;
         this.nameProduct = nameProduct;
         this.price = Comon.formatMoney(price);
-        this.addButton = addButton;
-        this.editButton = editButton;
-
         this.initComponents();
     }
 
     private void initComponents() {
-        this.setPreferredSize(new DimensionUIResource(200, 200));
         this.setLayout(null);
+        this.setFocusable(true);
+        this.setPreferredSize(new Dimension(150, 200));
 
         // Ảnh sản phẩm
         JLabel imageLabel = new JLabel();
         imageLabel.setIcon(new ImageIcon(imageProduct.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH)));
-        imageLabel.setBounds(50, 10, 100, 100); // Căn giữa theo chiều ngang
+        imageLabel.setBounds(25, 10, 100, 100); // Căn giữa theo chiều ngang
         this.add(imageLabel);
 
-        // Tên sản phẩm
-        JLabel nameLabel = new JLabel(nameProduct, SwingConstants.LEFT);
-        nameLabel.setFont(new Font("Arial", Font.BOLD, 14));
-        nameLabel.setBounds(10, 110, 180, 20);
-        this.add(nameLabel);
-
-        // Giá sản phẩm
-        JLabel priceLabel = new JLabel(price, SwingConstants.LEFT);
-        priceLabel.setFont(new Font("Arial", Font.PLAIN, 12));
-        priceLabel.setBounds(10, 130, 180, 20);
-        this.add(priceLabel);
-
-        // Nút chức năng
-        addButton.setBounds(10, 160, 85, 30);
-        editButton.setBounds(105, 160, 85, 30);
-        this.add(addButton);
-        this.add(editButton);
+        
     }
 }
--- src/GUI/panels/FoodPanel.java
+++ src/GUI/panels/FoodPanel.java
@@ -2,14 +2,15 @@ package GUI.panels;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+
 import GUI.Card.FoodCard;
 import GUI.Components.CustomButton;
 import GUI.Components.CustomCombobox;
-import Utils.Helper.CreateComponent;
 import GUI.Components.CustomPanel;
 import GUI.Components.CustomScrollPane;
 import GUI.Components.CustomTextField;
-import GUI.Components.CustomButton;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,34 +18,36 @@ import java.util.List;
 public class FoodPanel extends JPanel {
     private JPanel foodPanel;
     private List<FoodCard> foodCards;
-
-    private CardLayout cardLayout;
-    private JPanel buttonControllPanel;
+    private JPanel titlePanel;
     private CustomPanel filterPanel;
-    private CustomPanel showInfoPanel;
+    private CustomPanel buttonFilterPanel;
+    private CustomTextField searchField;
+    private CustomCombobox<String> statusComboBoxProduct;
+    private CustomCombobox<String> statusComboBoxCategory;
 
     public FoodPanel() {
-        this.foodCards = new ArrayList<>(); // Khởi tạo danh sách để tránh NullPointerException
+        this.foodCards = new ArrayList<>();
         this.initComponents();
     }
 
     private void initComponents() {
         this.setLayout(null);
-        this.setBackground(Color.WHITE);
+        // this.setBackground(Color.WHITE);
 
-        buttonControllPanel = createControlButtonPanel();
-        filterPanel = createFilterPanel();
-        foodPanel = createFoodPanel();
+        this.titlePanel = this.createControlButtonPanel();
+        this.filterPanel = this.createFilterPanel();
+        this.foodPanel = this.createFoodPanel();
+        this.buttonFilterPanel = this.createFilterButtonPanel();
 
-        buttonControllPanel.setBounds(10, 0, 1080, 100);
+        this.titlePanel.setBounds(10, 0, 1080, 80);
+        this.filterPanel.setBounds(10, 90, 1080, 80);
+        this.foodPanel.setBounds(10, 180, 1080, 480);
+        this.buttonFilterPanel.setBounds(10, 670, 1080, 50);
 
-        filterPanel.setBounds(10, 110, 1080, 80);
-
-        foodPanel.setBounds(10, 200, 1080, 600);
-
-        this.add(buttonControllPanel);
-        this.add(filterPanel);
-        this.add(foodPanel);
+        this.add(this.titlePanel);
+        this.add(this.filterPanel);
+        this.add(this.foodPanel);
+        this.add(this.buttonFilterPanel);
     }
 
     private JPanel createControlButtonPanel() {
@@ -53,54 +56,57 @@ public class FoodPanel extends JPanel {
 
         JLabel titlePanel = new JLabel("QUẢN LÝ ĐỒ ĂN");
         titlePanel.setFont(new Font("Sans-serif", Font.PLAIN, 30));
-        titlePanel.setBounds(420, 30, 400, 50);
+        titlePanel.setBounds(420, 20, 400, 50);
 
         panel.add(titlePanel);
-
         return panel;
     }
 
     private CustomPanel createFilterPanel() {
         CustomPanel panel = new CustomPanel();
         panel.setLayout(null);
+        panel.setBackground(Color.WHITE);
 
         JLabel searchLabel = new JLabel("Tìm kiếm:");
         searchLabel.setBounds(20, 20, 80, 35);
         panel.add(searchLabel);
 
-        CustomTextField searchField = new CustomTextField();
-        searchField.setBounds(100, 20, 200, 35);
-        panel.add(searchField);
+        this.searchField = new CustomTextField("Nhập thông tin sản phẩm");
+        this.searchField.setBounds(100, 20, 200, 35);
+        this.searchField.addFocusListener(new FocusListener() {
+            public String text = searchField.getText();
+            @Override
+            public void focusGained(FocusEvent e) {
+                if (this.text.equalsIgnoreCase("Nhập thông tin sản phẩm")) {
+                    searchField.setText("");
+                }
+            }
+            @Override
+            public void focusLost(FocusEvent e) {
+                if (!this.text.equalsIgnoreCase("")) {
+                    searchField.setText("Nhập thông tin sản phẩm");
+                }
+            }
+        });
+        panel.add(this.searchField);
 
-        JLabel statusLabel = new JLabel("Trạng thái:");
-        statusLabel.setBounds(320, 20, 80, 35);
-        panel.add(statusLabel);
+        JLabel statusProductLabel = new JLabel("Trạng thái:");
+        statusProductLabel.setBounds(320, 20, 80, 35);
+        panel.add(statusProductLabel);
 
-        String[] statuses = {"Tất cả", "Còn hàng", "Hết hàng"};
-        CustomCombobox<String> statusComboBox = new CustomCombobox<>(statuses);
-        statusComboBox.setBounds(400, 20, 150, 35);
-        panel.add(statusComboBox);
+        String[] statusesProduct = {"Tất cả", "Còn hàng", "Hết hàng"};
+        this.statusComboBoxProduct = new CustomCombobox<>(statusesProduct);
+        this.statusComboBoxProduct.setBounds(400, 20, 150, 35);
+        panel.add(this.statusComboBoxProduct);
 
-        CustomButton filterButton = new CustomButton("Lọc");
-        filterButton.setBounds(600, 20, 100, 35);
-        filterButton.setBackground(Color.decode("#03A9F4"));
-        filterButton.setForeground(Color.WHITE);
-        filterButton.setBorderColor(Color.decode("#03A9F4"));
-        panel.add(filterButton);
+        JLabel statusCategoryLabel = new JLabel("Loại sản phẩm:");
+        statusCategoryLabel.setBounds(570, 20, 100, 35);
+        panel.add(statusCategoryLabel);
 
-        CustomButton resetButton = new CustomButton("Đặt lại");
-        resetButton.setBounds(720, 20, 100, 35);
-        resetButton.setBackground(Color.RED);
-        resetButton.setForeground(Color.WHITE);
-        resetButton.setBorderColor(Color.RED);
-        panel.add(resetButton);
-
-        CustomButton addNewButton = new CustomButton("Thêm");
-        addNewButton.setBounds(840, 20, 100, 35);
-        addNewButton.setBackground(Color.decode("#388E3C"));
-        addNewButton.setForeground(Color.WHITE);
-        addNewButton.setBorderColor(Color.decode("#388E3C"));
-        panel.add(addNewButton);
+        String[] statusesCategory = {"Tất cả"};
+        this.statusComboBoxCategory = new CustomCombobox<>(statusesCategory);
+        this.statusComboBoxCategory.setBounds(670, 20, 150, 35);
+        panel.add(this.statusComboBoxCategory);
 
         return panel;
     }
@@ -112,18 +118,11 @@ public class FoodPanel extends JPanel {
         JPanel tableDataPanel = new CustomPanel();
         tableDataPanel.setBackground(Color.WHITE);
 
-        int columns = 5;
+        int columns = 6;
         tableDataPanel.setLayout(new GridLayout(0, columns, 10, 10));
 
-        for (int i = 0; i < 20; i++) {
-            FoodCard foodCard = new FoodCard(
-                    new ImageIcon(System.getProperty("user.dir") + "/src/Assets/Food/none.png"),
-                    "Thức ăn: " + i,
-                    "200000",
-                    new CustomButton("Sửa"),
-                    new CustomButton("Xóa"));
-            foodCards.add(foodCard);
-            tableDataPanel.add(foodCard);
+        for (int i = 0; i < 12; i++) {
+
         }
         JPanel lastRow = new JPanel();
         lastRow.setBackground(Color.WHITE);
@@ -132,7 +131,6 @@ public class FoodPanel extends JPanel {
         JScrollPane scrollPane = new CustomScrollPane(tableDataPanel);
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-
         scrollPane.getVerticalScrollBar().setUnitIncrement(16);
         scrollPane.setBorder(null);
 
@@ -140,5 +138,27 @@ public class FoodPanel extends JPanel {
         return panel;
     }
 
+    private CustomPanel createFilterButtonPanel() {
+        CustomPanel panel = new CustomPanel();
+        panel.setLayout(null);
+        panel.setBackground(Color.WHITE);
 
+        CustomButton addButton = new CustomButton("Thêm");
+        addButton.setBounds(200, 10, 100, 30);
+        panel.add(addButton);
+
+        CustomButton editButton = new CustomButton("Sửa");
+        editButton.setBounds(320, 10, 100, 30);
+        panel.add(editButton);
+
+        CustomButton deleteButton = new CustomButton("Xóa");
+        deleteButton.setBounds(440, 10, 100, 30);
+        panel.add(deleteButton);
+
+        CustomButton detailButton = new CustomButton("Xem chi tiết");
+        detailButton.setBounds(560, 10, 120, 30);
+        panel.add(detailButton);
+
+        return panel;
+    }
 }
