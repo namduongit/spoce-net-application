078ad333d5c60ca00bd82436bf9afed69213dc5e
congquan92 <145172432+congquan92@users.noreply.github.com>
2025-05-07 21:07:23 +0700

    temp

--- src/Utils/Config/ConfigSQL.java
+++ src/Utils/Config/ConfigSQL.java
@@ -14,8 +14,8 @@ public class ConfigSQL {
     public static String URL = "jdbc:mysql://159.89.207.183:3306/net_gaming_management";
     // Kết nối Local
     public static String USER_NAMEServer = "root";
-    public static String PASSWORDServer = "NDuong205";
-    public static String URLServer = "jdbc:mysql://localhost:3306/net_gaming_management";
+    public static String PASSWORDServer = "1234567";
+    public static String URLServer = "jdbc:mysql://localhost:3307/net_gaming_management";
 
 
 
